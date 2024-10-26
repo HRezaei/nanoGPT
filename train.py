31837d5bf67b2eccdc2c510aae4c92c64aed9b5b
hrezaei <mr.hrezaei@gmail.com>
2024-10-26 05:57:12 +0100

    Add codes to the wandb log

--- train.py
+++ train.py
@@ -245,7 +245,13 @@ def get_lr(it):
 # logging
 if wandb_log and master_process:
     import wandb
-    wandb.init(project=wandb_project, name=wandb_run_name, config=config)
+    wandb.init(
+        project=wandb_project,
+        name=wandb_run_name,
+        config=config,
+        save_code=True,
+        settings=wandb.Settings(code_dir="."),
+    )
 
 # training loop
 X, Y = get_batch('train') # fetch the very first batch
