a51c7962e3541ca96bb83c5e83802135f77954e2
hrezaei <mr.hrezaei@gmail.com>
2024-10-26 15:24:45 +0100

    Add Slurm job id to wandb log for reference

--- train_la.py
+++ train_la.py
@@ -277,6 +277,7 @@ if wandb_log and master_process:
     import wandb
     config_for_wandb = config.copy()
     config_for_wandb["Slurm_job_file_content"] = os.environ.get("SLRUM_JOB_FILE_CONTENT", "not set")
+    config_for_wandb["Slurm_job_id"] = os.environ.get("SLURM_JOB_ID", "not set")
     wandb.init(
         project=wandb_project,
         name=wandb_run_name,
